2967db51baab8c9b3dc5694266093a4693d00b53
Jetarucode <jetaruphillips@gmail.com>
2022-08-02 17:25:14 -0600

    fixed bugs 💀 and added missing using derivatives

--- main.cs
+++ main.cs
@@ -1,38 +1,44 @@
-public class Program
+using Discord;
+using Discord.WebSocket;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+
+namespace main {
+    class Program
 {
-	public static Task Main(string[] args) => new Program().MainAsync();
 
-	public async Task MainAsync()
-	{
-	}
+    public static Task Main(string[] args) => new Program().MainAsync();
 
-private Task Log(LogMessage msg)
-{
-	Console.WriteLine(msg.ToString());
-	return Task.CompletedTask;
-}
+    private DiscordSocketClient _client;
 
-private DiscordSocketClient _client;
+    public async Task MainAsync()
+    {
+        _client = new DiscordSocketClient();
 
-public async Task MainAsync()
-{
-    _client = new DiscordSocketClient();
+        _client.Log += Log;
 
-    _client.Log += Log;
+        //  You can assign your bot token to a string, and pass that in to connect.
+        //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
+        var token = File.ReadAllText("token.txt");
 
-    //  You can assign your bot token to a string, and pass that in to connect.
-    //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
-    var token = File.ReadAllText("token.txt");
+        // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
+        // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
+        // var token = File.ReadAllText("token.txt");
+        // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
 
-    // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
-    // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
-    // var token = File.ReadAllText("token.txt");
-    // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
 
-    await _client.LoginAsync(TokenType.Bot, token);
-    await _client.StartAsync();
+        // Block this task until the program is closed.
+        await Task.Delay(-1);
 
-    // Block this task until the program is closed.
-    await Task.Delay(-1);
     }
-}
+        private Task Log(LogMessage msg)
+        {
+	        Console.WriteLine(msg.ToString());
+	        return Task.CompletedTask;
+        }
+    }
+}   
