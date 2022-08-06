3aed1a9f95a2f2bf05046fce971d594a8b1f12b2
muhammad fadhlan // ThatNerd <muhammadfadhlan4@gmail.com>
2022-08-06 10:14:11 +0700

    Added .gitignore for .vs and obj folders Added first storage implementation of FlatStorage with abstract interface Created sln file
    please dont kill me for this.

---
+++ Storage/Storage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OriBot.Storage
+{
+    public abstract class Storage
+    {
+        protected Storage()
+        {
+            
+        }
+
+
+        public abstract T? Get<T>(string scope, string key, out bool success);
+        public abstract void Set<T>(string scope, string key, T value);
+
+        public abstract Task CleanupAsync();
+
+        public abstract bool Exists(string scope, string key);
+
+        public abstract bool Remove(string scope, string key);
+
+        public abstract string[] GetScopes();
+
+        public abstract string[] GetKeys(string scope);
+
+    }
+}
