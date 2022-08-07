08ee65914db0aa11128699e97e5f5091ec625592
muhammad fadhlan // ThatNerd <muhammadfadhlan4@gmail.com>
2022-08-07 10:26:14 +0700

    Object based storage similar to JSON using dynamic

---
+++ Storage2/ObjectStorage.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace OriBot.Storage2
+{
+    public class JObject
+    {
+        [JsonProperty]
+        private Dictionary<dynamic, dynamic> _storage;
+
+        [JsonProperty]
+        private bool strict = false;
+
+        public JObject(bool strict = false)
+        {
+            this.strict = strict;
+        }
+
+        [JsonConstructor]
+        public JObject(Dictionary<dynamic, dynamic> starterobject, bool strict = false)
+        {
+            this.strict = strict;
+            this._storage = starterobject;
+        }
+
+        public dynamic this[dynamic key]
+        {
+            get
+            {
+                if (_storage is null)
+                {
+                    _storage = new();
+                }
+                if (!_storage.TryGetValue(key,out dynamic _))
+                {
+                    if (strict)
+                    {
+                        Console.WriteLine("WARNING: Null returned when trying to get key " + key + ", a crash may be happening. Disable Strict mode to prevent this from happening");
+                        return null;
+                    }
+                    _storage[key] = new JObject(false);
+                }
+                return _storage[key];
+            }
+            set
+            {
+                if(_storage is null)
+                {
+                    _storage = new();
+                }
+                _storage[key] = value;
+            }
+        }
+
+        public string Save()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.None);
+        }
+
+        public static JObject Load(string obj)
+        {
+            return JsonConvert.DeserializeObject<JObject>(obj);
+        }
+
+        public override string ToString()
+        {
+            return Save();
+        }
+
+    }
+}
