07f042361d283a17c5575d72da1fc76c24ef8aca
Muhammad Fadhlan // thatnerd527 <muhammadfadhlan4@gmail.com>
2023-06-17 22:04:05 +0700

    Structure

---
+++ PassiveHandlers/PassiveHandler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Discord.WebSocket;
+
+namespace OriBot.PassiveHandlers
+{
+    public abstract class PassiveHandler
+    {
+        public readonly DiscordSocketClient _client;
+        public readonly SocketUserMessage _message;
+        
+        /// <summary>
+        /// Passive handlers work in a pretty similar way to how commands work in the discord.net framework. 
+        /// In that anytime that a passive handler is run, The constructor for the passive handler class is instantiated. 
+        /// After the constructor is run. The Run method of the passive handler is called. Which is what defines what the passive handler does. 
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="message"></param>
+        public PassiveHandler(DiscordSocketClient client, SocketUserMessage message)
+        {
+            _client = client;
+            _message = message;
+            Run();
+        }
+        private async Task _Run()
+        {
+            Run();
+        }
+
+        public abstract Task Run();
+
+
+    }
+}
--- main.cs
+++ main.cs
@@ -1,44 +1,69 @@
 using Discord;
 using Discord.WebSocket;
+using Discord.Commands;
+using Discord.Commands.Builders;
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using OriBot.Commands;
+using OriBot.PassiveHandlers;
+using OriBot.Storage2;
+using System.Collections.Generic;
+using OriBot.Framework;
 
-
-namespace main {
-    class Program
+namespace main
 {
-
-    public static Task Main(string[] args) => new Program().MainAsync();
-
-    private DiscordSocketClient _client;
-
-    public async Task MainAsync()
+    public class Memory
     {
-        _client = new DiscordSocketClient();
-
-        _client.Log += Log;
-
-        //  You can assign your bot token to a string, and pass that in to connect.
-        //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
-        var token = File.ReadAllText("token.txt");
-
-        // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
-        // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
-        // var token = File.ReadAllText("token.txt");
-        // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
-
-        await _client.LoginAsync(TokenType.Bot, token);
-        await _client.StartAsync();
-
-        // Block this task until the program is closed.
-        await Task.Delay(-1);
-
+        public static Dictionary<ulong,GeneralServerContext> contextStorage = new();
     }
+
+
+    internal class Program
+    {
+        public static Task Main(string[] args) => new Program().MainAsync();
+
+        private DiscordSocketClient _client;
+
+        private CommandSystem _commandhub;
+
+        private PassiveHandlerHub _passiveHandlerHub;
+
+        public async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+
+            //  You can assign your bot token to a string, and pass that in to connect.
+            //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
+            var token = File.ReadAllText("token.txt");
+
+            // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
+            // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
+            // var token = File.ReadAllText("token.txt");
+            // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+            Console.WriteLine(JObject.Load(File.ReadAllText("test.json")).ToString());
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+            await InitializeOtherSystems();
+            main.Memory.contextStorage.Add(1005355539447959552, new OricordContext());
+            // Block this task until the program is closed.
+            await Task.Delay(-1);
+        }
+
+        public async Task InitializeOtherSystems()
+        {
+            _commandhub = new OriBot.Commands.CommandSystem(_client, new CommandService());
+            await _commandhub.RegisterCommandsAsync();
+            _passiveHandlerHub = new PassiveHandlerHub(_client);
+            _passiveHandlerHub.RegisterPassiveHandlers();
+        }
+
         private Task Log(LogMessage msg)
         {
-	        Console.WriteLine(msg.ToString());
-	        return Task.CompletedTask;
+            Console.WriteLine(msg.ToString());
+
+            return Task.CompletedTask;
         }
     }
-}   
+}
