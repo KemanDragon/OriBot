2fc9fc36554ea02c3e575a913fb0ccf3347b9bc9
IamMU <gamemudev010@gmail.com>
2023-07-22 11:41:14 +0200

    ignore

---
+++ Utilities/Color.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Utilities
+{
+    /// <summary>
+    /// RGB Colors for terminals that support 24-bit colors
+    /// </summary>
+    public class Color
+    {
+        /* ********* **
+        ** VARIABLES **
+        ** ********* */
+        private String ansiCodeTemplate = "\u001b[38;2;r;g;bm";
+        private static String ansiCodeReset = "\u001b[0m";
+        private String ansiCode;
+
+
+        /* *********** **
+        ** CONSTRUCTOR **
+        ** *********** */
+        public Color(int r, int g, int b)
+        {
+            ansiCode = ansiCodeTemplate.Replace("r", r.ToString()).Replace("g", g.ToString()).Replace("b", b.ToString());
+        }
+
+
+        /* ******* **
+        ** METHODS **
+        ** ******* */
+
+        /// <summary>
+        /// Returns an ANSI Code to reset the terminal's colors
+        /// </summary>
+        /// <returns>ANSI Code</returns>
+        public static String Reset()
+        {
+            return ansiCodeReset;
+        }
+
+        public override String ToString()
+        {
+            return ansiCode;
+        }
+    }
+}
