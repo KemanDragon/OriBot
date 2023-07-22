6bcf56fff81246529f49d5b1a8d1127c98c8a17d
IamMU <gamemudev010@gmail.com>
2023-07-22 09:46:50 +0200

    test

--- main.cs
+++ main.cs
@@ -3,42 +3,57 @@ using Discord.WebSocket;
 using System;
 using System.IO;
 using System.Threading.Tasks;
+using Oribot.Utilities;
 
 
 namespace main {
     class Program
-{
-
-    public static Task Main(string[] args) => new Program().MainAsync();
-
-    private DiscordSocketClient _client;
-
-    public async Task MainAsync()
     {
-        _client = new DiscordSocketClient();
+        static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
 
-        _client.Log += Log;
+        //private DiscordSocketClient _client;
+        //private Logger logger = new Logger();
 
-        //  You can assign your bot token to a string, and pass that in to connect.
-        //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
-        var token = File.ReadAllText("token.txt");
-
-        // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
-        // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
-        // var token = File.ReadAllText("token.txt");
-        // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
-
-        await _client.LoginAsync(TokenType.Bot, token);
-        await _client.StartAsync();
-
-        // Block this task until the program is closed.
-        await Task.Delay(-1);
-
-    }
-        private Task Log(LogMessage msg)
+        public async Task MainAsync()
         {
-	        Console.WriteLine(msg.ToString());
-	        return Task.CompletedTask;
+            //logger.Log("Some logs");
+            //logger.Warn("Some warning");
+            //logger.Error("Some error");
+
+            // Creating config
+            //var _config = new DiscordSocketConfig {
+            //    MessageCacheSize = 100
+            //};
+
+            // Creating the client
+            //_client = new DiscordSocketClient(_config);
+
+            ////_client.Log += Log;
+
+            //// Start the bot
+            //await _client.LoginAsync(TokenType.Bot, "MTExNzg4ODQ4MjU0Nzg2Mzc2Mw.Go5KNk.EQ2_8YMjyWXuWLY4XW6ONfF8VUOZYa9PBcxpJA");//Environment.GetEnvironmentVariable("TOKEN"));
+            //await _client.StartAsync();
+
+            //_client.MessageUpdated += MessageUpdated;
+            //_client.Ready += BotReady; 
+                
+            //// Block this task until the program is closed.
+            //await Task.Delay(-1);
         }
+        
+     //   private Task BotReady()
+     //   {
+     //       logger.Log("Bot is ready");
+            
+     //       return Task.CompletedTask;
+     //   }
+
+     //   private async Task MessageUpdated(Cacheable<IMessage, ulong> before, SocketMessage after, ISocketMessageChannel channel)
+	    //{
+		   // // If the message was not in the cache, downloading it will result in getting a copy of `after`.
+		   // var message = await before.GetOrDownloadAsync();
+		   // Console.WriteLine($"{message} -> {after}");
+	    //}
+
     }
 }   
